73b9162b63d0d915a9ae8da4bf40eded6bf381cc
Megan Wu <meganwu1994@gmail.com>
2019-06-28 13:57:32 -0500

    got rid of inquirer

--- greatBay.js
+++ greatBay.js
@@ -23,42 +23,17 @@ connection.connect(function(err) {
 });
 
 
-// Created a series of questions
-inquirer.prompt([
+function updateBid() {
 
-    {
-      type: "input",
-      name: "name",
-      message: "What's your name ?"
-    },
-  
-    {
-      type: "list",
-      name: "items",
-      message: "What items do you want to bid ?",
-      choices: ["item1", "item2", "item3"]
-    },
-  
-    {
-      type: "input",
-      name: "bid",
-      message: "How much do you want to bid ?"
+    if(bid > highest_bid) {
+        highest_bid = bid;
     }
-  
-  ]).then (function updateBid() {
-    var query = connection.query(
-      "What item do you want to bid ?",
-      [
-        {
-          highest_bid: 
-        }
-      ],
-      function(err, res) {
+        function(err, res) {
         if (err) throw err;
         console.log(res.affectedRows + " bid updated!\n");
-      }
+        }
     );
 
     // logs the actual query being run
     console.log(query.sql);
-});
+};
